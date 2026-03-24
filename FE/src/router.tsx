49b38f02083fde08ba442752eaabcce3e0a8aace
신건하 <gunha1215@gmail.com>
2026-03-24 20:40:32 +0900

    fix/ hairlist api 연결

--- FE/src/router.tsx
+++ FE/src/router.tsx
@@ -126,16 +126,16 @@ function createProtectedRoute<
     getParentRoute: () => rootRoute,
     path,
     validateSearch,
-    // beforeLoad: async ({ context, location }) => {
-    //   if (!(await context.auth.ensureAuthenticated())) {
-    //     throw redirect({
-    //       to: '/auth/login',
-    //       search: {
-    //         redirect: location.href,
-    //       },
-    //     })
-    //   }
-    // },
+    beforeLoad: async ({ context, location }) => {
+      if (!(await context.auth.ensureAuthenticated())) {
+        throw redirect({
+          to: '/auth/login',
+          search: {
+            redirect: location.href,
+          },
+        })
+      }
+    },
     component,
   })
 }
